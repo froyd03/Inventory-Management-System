1bc93fb499906c5813bd13dd46bd751c98e3b715
Froyd <banataofroyd@gmail.com>
2025-02-11 12:20:24 +0800

    final login page and mobile view responsive

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -4,14 +4,24 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 import HttpsOutlinedIcon from '@mui/icons-material/HttpsOutlined';
 import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
 import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import logoBrand from '../../assets/logo.png'
+import { useEffect, useState } from 'react';
 
 export default function Login(){
+
+    const [isShowPassword, setShowPassword] = useState(false);
+    const [toText, setToText] = useState("password");
+    function handleShowPassword(){
+        setShowPassword(p => !p);
+    }
+
     return(
         <section>
             <div className="logo">
-                <HttpsOutlinedIcon /> 
+               <img src={logoBrand} width='200px' alt="" />
             </div>
             <div className="login">
+               <img src={logoBrand} width='60px' alt="" />
                 <h2>Log in to your account</h2>
                 <p>Welcome back! Please enter your details.</p>
                 <div className="inputFields">
@@ -23,10 +33,13 @@ export default function Login(){
                     <label>Password</label>
                     <div className="inp-container">
                         <HttpsOutlinedIcon color='primary'/>
-                        <input type="password" placeholder='password'/>
-                        <RemoveRedEyeOutlinedIcon />
+                        <input type={isShowPassword ? "text":"password"} placeholder='Password'/>
+                        {isShowPassword ? <RemoveRedEyeOutlinedIcon onClick={handleShowPassword} />  : 
+                                          <VisibilityOffOutlinedIcon onClick={handleShowPassword } />}
                     </div>
                 </div>
+                <button className='sign-in'>Sign in</button>
+                <button className='createAcc'>Create Account</button>
             </div>
         </section>
     )
