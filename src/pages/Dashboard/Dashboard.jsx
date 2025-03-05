ad5a0599dce4abedc712abc2c89da3d0ad795107
Froyd <banataofroyd@gmail.com>
2025-03-05 14:38:10 +0800

    dashboard page done

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -8,12 +8,41 @@ import retrn from '../../assets/return-on-investmentC.png';
 import purchase from '../../assets/checklistC.png';
 import box from '../../assets/boxC.png';
 import delivery from '../../assets/deliveryC.png';
+import {dataGraphYearly, dataGraphWeekly} from '../../utils/dataGraph'
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import ListAltOutlinedIcon from '@mui/icons-material/ListAltOutlined';
 import { BarChart } from '@mui/x-charts/BarChart';
 import logo from '../../assets/logo.png'
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Dashboard(){
+
+    function handleSelectDate(e){
+        console.log(e.target.value);
+    }
+
+    const containerRef = useRef(null);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            
+            if (containerRef.current) {
+                containerRef.current.scrollBy({ 
+                    left: containerRef.current.clientWidth,  
+                });
+                if (
+                    containerRef.current.scrollLeft + containerRef.current.clientWidth >= 
+                    containerRef.current.scrollWidth
+                ) {
+                    setTimeout(() => {
+                    containerRef.current.scrollTo({ left: 0 });
+                    }, 100);
+                }
+            }
+        }, 3000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <section>
             <div className="container">
@@ -23,7 +52,7 @@ export default function Dashboard(){
                         <div className="item">
                             <p>Sales</p>
                             <div className='sales'>
-                                <img width={'30px'} height={'30px'} src={sales}/>
+                                <img width={'30px'} alt='error' height={'30px'} src={sales}/>
                                 <h4>₱ 00.00</h4>
                             </div>
                         </div>
@@ -31,7 +60,7 @@ export default function Dashboard(){
                         <div className="item">
                             <p>Revenue</p>
                             <div className='sales'>
-                                <img width={'30px'} height={'30px'} src={revenue}/>
+                                <img width={'30px'} alt='error' height={'30px'} src={revenue}/>
                                 <h4>₱ 00.00</h4>
                             </div>
                         </div>
@@ -39,7 +68,7 @@ export default function Dashboard(){
                         <div className="item">
                             <p>Profit</p>
                             <div className='sales'>
-                                <img width={'30px'} height={'30px'} src={profit}/>
+                                <img width={'30px'} alt='error' height={'30px'} src={profit}/>
                                 <h4>₱ 00.00</h4>
                             </div>
                         </div>
@@ -49,13 +78,13 @@ export default function Dashboard(){
                     <h3>Inventory Summary</h3>
                     <div className="item-summary">
                         <div className="item">
-                            <img width={'30px'} src={box}/>
+                            <img width={'30px'} alt='error' src={box}/>
                             <p><b>0</b></p>
                             <p>Quantity in Hand</p>
                         </div>
                         <span className="vl"></span>
                         <div className="item">
-                            <img width={'30px'} src={delivery}/>
+                            <img width={'30px'} alt='error' src={delivery}/>
                             <p><b>0</b></p>
                             <p>To be recieve</p>
                         </div>
@@ -69,7 +98,7 @@ export default function Dashboard(){
                         <div className="item">
                             <p>Purchase</p>
                             <div className='sales'>
-                                <img width={'40px'} height={'40px'} src={purchase}/>
+                                <img width={'40px'} alt='error' height={'40px'} src={purchase}/>
                                 <h4>₱ 00.00</h4>
                             </div>
                         </div>
@@ -77,7 +106,7 @@ export default function Dashboard(){
                         <div className="item">
                             <p>Cost</p>
                             <div className='sales'>
-                                <img width={'30px'} height={'30px'} src={cost}/>
+                                <img width={'30px'} alt='error' height={'30px'} src={cost}/>
                                 <h4>₱ 00.00</h4>
                             </div>
                         </div>
@@ -85,7 +114,7 @@ export default function Dashboard(){
                         <div className="item">
                             <p>Return</p>
                             <div className='sales'>
-                                <img width={'30px'} height={'30px'} src={retrn}/>
+                                <img width={'30px'} alt='error' height={'30px'} src={retrn}/>
                                 <h4>₱ 00.00</h4>
                             </div>
                         </div>
@@ -110,34 +139,82 @@ export default function Dashboard(){
             </div>
             <div className="container">
                 <div className="layout">
-                    <h3>Sales & Purchase</h3>
-
+                    <div className="header">
+                        <h3>Sales & Purchase</h3>
+                        <div className="slctDate">
+                            <CalendarTodayOutlinedIcon />
+                            <select onChange={handleSelectDate}>
+                                <option value="Monthly">Weekly</option>
+                                <option value="Monthly">Monthly</option>
+                                <option value="Yearly">Yearly</option>
+                            </select>
+                        </div>
+                    </div>
                     <BarChart 
+                        dataset={dataGraphYearly}
                         xAxis={[
                             {
-                            data: ['day 1', 'day 2', 'day 3', 'day 4'],
+                            dataKey: 'date',
                             scaleType: 'band',
                             categoryGapRatio: 0.5,
                             barGapRatio: 0.3,
                             },
-                            
                         ]}
                         series={[
-                            {data: [4, 5, 3, 2], color: '#1E214C', label: 'purchase'}, 
-                            {data: [1, 2, 3, 8], color: '#409BBB', label: 'sales'},
+                            {dataKey: 'sales', color: '#1E214C', label: 'purchase'}, 
+                            {dataKey: 'purchase', color: '#409BBB', label: 'sales'},
                         ]}
                         borderRadius={20}
-                        BarLabel
                         style={{width:'90%', height:'300px'}}
                     />
                 </div>
                 <div className="top-sell">
                     <h3>Top Selling Product</h3>
-                    <div className="product">
-                        <img src={logo} width='100px' />
-                        <p>Product Name</p>
+                    <div className="items" ref={containerRef}>
+                        <div className="product">
+                            <h4>Top 1</h4>
+                            <img src={logo} alt='error' width='100px' />
+                            <p>Product Name</p>
+                        </div>
+                        <div className="product">
+                            <h4>Top 2</h4>
+                            <img src={logo} alt='error' width='100px' />
+                            <p>Product Name</p>
+                        </div>
+                        <div className="product">
+                            <h4>Top 3</h4>
+                            <img src={logo} alt='error' width='100px' />
+                            <p>Product Name</p>
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div className='lowStockItem'>
+                <h3>Low Quantity Stock</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Product ID</th>
+                            <th>Remaining Quantity</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>LCD</td>
+                            <td>1234</td>
+                            <td>10 packets</td>
+                            <td><label className='warning'>Low</label></td>
+                        </tr>
+                        <tr>
+                            <td>LCadsasdadD</td>
+                            <td>1234</td>
+                            <td>10 packets</td>
+                            <td><label className='warning'>Low</label></td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </section>
     )
---
+++ src/utils/dataGraph.js
@@ -0,0 +1,102 @@
+
+
+export const dataGraphYearly = [
+    {
+        date: 'Jan',
+        purchase: 2,
+        sales: 5,
+    },
+    {
+        date: 'Feb',
+        purchase: 9,
+        sales: 7,
+    },
+    {
+        date: 'Mar',
+        purchase: 2,
+        sales: 8,
+    },
+    {
+        date: 'Apr',
+        purchase: 7,
+        sales: 6,
+    },
+    {
+        date: 'May',
+        purchase: 9,
+        sales: 9,
+    },
+    {
+        date: 'June',
+        purchase: 2,
+        sales: 1,
+    },
+    {
+        date: 'July',
+        purchase: 5,
+        sales: 1,
+    },
+    {
+        date: 'Aug',
+        purchase: 1,
+        sales: 3,
+    },
+    {
+        date: 'Sept',
+        purchase: 7,
+        sales: 4,
+    },
+    {
+        date: 'Oct',
+        purchase: 2,
+        sales: 3,
+    },
+    {
+        date: 'Nov',
+        purchase: 8,
+        sales: 2,
+    },
+    {
+        date: 'Dec',
+        purchase: 7,
+        sales: 4,
+    },
+]
+
+export const dataGraphWeekly = [
+    {
+        date: 'Mon',
+        purchase: 7,
+        sales: 4,
+    },
+    {
+        date: 'Tue',
+        purchase: 7,
+        sales: 4,
+    },
+    {
+        date: 'Wed',
+        purchase: 7,
+        sales: 4,
+    },
+    {
+        date: 'Thur',
+        purchase: 7,
+        sales: 4,
+    },
+    {
+        date: 'Fri',
+        purchase: 7,
+        sales: 4,
+    },
+    {
+        date: 'Sat',
+        purchase: 7,
+        sales: 4,
+    },
+    {
+        date: 'Sun',
+        purchase: 7,
+        sales: 4,
+    },
+]
